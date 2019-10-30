9a6b8d551645782baf28c200eb26f4106d7fc12e
181221 <pederwaa@ifi.uio.no>
2019-10-30 18:16:58 +0100

    start assignment-e

--- obligatorisk/assignment-e/ranking.py
+++ obligatorisk/assignment-e/ranking.py
@@ -4,7 +4,7 @@
 from abc import ABC, abstractmethod
 from corpus import Corpus
 from invertedindex import Posting, InvertedIndex
-from math import log
+import math
 
 
 class Ranker(ABC):
@@ -73,35 +73,19 @@ class BetterRanker(Ranker):
         self._corpus = corpus
         self._inverted_index = inverted_index
         self._dynamic_score_weight = 1.0  # TODO: Make this configurable.
-        self._static_score_weight = 1.0  # TODO: Make this configurable. #gd
+        self._static_score_weight = 1.0  # TODO: Make this configurable.
         self._static_score_field_name = "static_quality_score"  # TODO: Make this configurable.
-        self._dyn = 0.0
 
     def reset(self, document_id: int) -> None:
         self._score = 0.0
         self._document_id = document_id
 
     def update(self, term: str, multiplicity: int, posting: Posting) -> None:
-        df = self._inverted_index.get_document_frequency(term)
-        """
-        A word occurring in a large proportion of documents is not a good discriminator, that is why we use 
-        inverse document frequency. The idea is to scale down the terms with high collection frequency, thus the idf 
-        of a rare term is high, whereas the idf of a frequent term is likely to be low 
-        """
-        idf_score = self.idf(df)
-        """
-        Term Frequency - how many times does the term occour in a document.
-        If a word is occourring multiple teams in a documents it is more relevant. 
-        Term frequency x Inverse document frequency tend to filter out common terms
-        """
-        tf_idf_score = multiplicity * posting.term_frequency * idf_score
-        self._score += tf_idf_score
-
-    def idf(self, df):
-        return log((self._corpus.size()/df), 10)
+        tf_score = math.log10(multiplicity * (1 + posting.term_frequency))
+        idf_score = math.log10(self._corpus.size() / float(self._inverted_index.get_document_frequency(term)))
+        self._score += tf_score * idf_score
 
     def evaluate(self) -> float:
         document = self._corpus[self._document_id]
         static_quality_score = float(document[self._static_score_field_name] or 0.0)
-        return static_quality_score + self._score
-
+        return (self._dynamic_score_weight * self._score) + (self._static_score_weight * static_quality_score)
